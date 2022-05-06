df3eed7ac7c39b74114c53d86921730bc14fe36d
Guillaume RYCKAERT <gryckaert@ipsosenso.com>
2022-05-06 14:58:54 +0200

    Add gitignore & local settings

--- tmcro_api/settings.py
+++ tmcro_api/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k#euzb9+q(sx^xao^wk1s%1r-_=nih*p(sin*=q1d+vg4!41i%'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -122,3 +122,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+from .settings_local import *
